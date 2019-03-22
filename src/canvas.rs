38b4c06939a6a1ca0b1007183dcccaa4d7f9a6b8
Wes Weirather <weirdev@outlook.com>
2019-03-21 23:18:27 -0500

    Added ability to draw ellipses as well as lines.

--- src/canvas.rs
+++ src/canvas.rs
@@ -9,13 +9,22 @@ pub struct Line {
     pub y1: f64
 }
 
+pub struct Ellipse {
+    pub x0: f64,
+    pub y0: f64,
+    pub x1: f64,
+    pub y1: f64
+}
+
 pub enum Shape {
-    Line(Line)
+    Line(Line),
+    Ellipse(Ellipse)
 }
 
 pub trait Canvas {
     fn draw(&mut self, x: f64, y: f64, color: u8);
     fn draw_line(&mut self, line: &Line);
+    fn draw_ellipse(&mut self, ellipse: &Ellipse);
     fn save<Q>(&self, path: Q) -> io::Result<()> 
             where Q: AsRef<Path>;
     fn render(&mut self, scene: Vec<Shape>);
@@ -36,7 +45,8 @@ impl ImageCanvas {
 impl Canvas for ImageCanvas {
     fn draw(&mut self, x: f64, y: f64, color: u8) {
         let xcoord = (x * self.img.dimensions().0 as f64).round() as u32;
-        let ycoord = (y * self.img.dimensions().1 as f64).round() as u32;
+        // image y axis 0 at top, but for Canvas we use bottom as 0
+        let ycoord = ((1.0 - y) * self.img.dimensions().1 as f64).round() as u32;
         self.img.put_pixel(xcoord, ycoord, Luma([color]));
     }
 
@@ -47,14 +57,19 @@ impl Canvas for ImageCanvas {
 
     fn draw_line(&mut self, line: &Line) {
         let dims = self.img.dimensions();
-        draw_line(line, self, dims);
+        draw_line_antialiased(line, self, dims);
     }
     
+    fn draw_ellipse(&mut self, ellipse: &Ellipse) {
+        let dims = self.img.dimensions();
+        draw_ellipse(ellipse, self, dims);
+    }
 
     fn render(&mut self, scene: Vec<Shape>) {
         for shape in scene.iter() {
             match shape {
-                Shape::Line(line) => self.draw_line(line)
+                Shape::Line(line) => self.draw_line(line),
+                Shape::Ellipse(ellipse) => self.draw_ellipse(ellipse)
             }
         }
     }
@@ -68,7 +83,7 @@ fn scale_to_byte(x: f64) -> u8 {
     (x * 255.0).round() as u8
 }
 
-fn draw_line(line: &Line, img: &mut impl Canvas, dimensions: (u32, u32)) {
+fn draw_line_antialiased(line: &Line, img: &mut impl Canvas, dimensions: (u32, u32)) {
     let mut x0 = line.x0 * dimensions.0 as f64;
     let mut y0 = line.y0 * dimensions.1 as f64;
     let mut x1 = line.x1 * dimensions.0 as f64;
@@ -142,4 +157,69 @@ fn draw_line(line: &Line, img: &mut impl Canvas, dimensions: (u32, u32)) {
             intery += gradient;
         }
     }
+}
+
+//http://members.chello.at/~easyfilter/bresenham.html
+fn draw_ellipse(ellipse: &Ellipse, img: &mut impl Canvas, dimensions: (u32, u32)) {
+    let mut x0 = (ellipse.x0 * dimensions.0 as f64).round() as u32;
+    let mut y0 = (ellipse.y0 * dimensions.1 as f64).round() as u32;
+    let mut x1 = (ellipse.x1 * dimensions.0 as f64).round() as u32;
+    let mut y1 = (ellipse.y1 * dimensions.1 as f64).round() as u32;
+
+    let mut a = (x1 as i32 - x0 as i32).abs() as u32;
+    let b = (y1 as i32 - y0 as i32).abs() as u32;
+    let mut b1 = b & 1;
+
+    let mut dx = 4*(1 - a as i32) * b as i32 * b as i32;
+    let mut dy = 4*(b1 as i32 + 1) * a as i32 * a as i32;
+    let mut err = dx + dy + (b1 as i32 * a as i32 * a as i32);
+
+    if x0 > x1 {
+        x0 = x1;
+        x1 += a;
+    }
+    if y0 > y1 {
+        y0 = y1;
+    }
+    y0 += (b+1) / 2;
+    y1 = y0 - b1;
+
+    a *= 8 * a;
+    b1 = 8 * b * b;
+
+    println!("{} {} {} {}", x0, x1, y0, y1);
+
+    loop {
+        img.draw(x1 as f64 / dimensions.0 as f64, y0 as f64 / dimensions.1 as f64, 255);
+        img.draw(x0 as f64 / dimensions.0 as f64, y0 as f64 / dimensions.1 as f64, 255);
+        img.draw(x0 as f64 / dimensions.0 as f64, y1 as f64 / dimensions.1 as f64, 255);
+        img.draw(x1 as f64 / dimensions.0 as f64, y1 as f64 / dimensions.1 as f64, 255);
+        let e2 = 2 * err;
+        if e2 <= dy {
+            y0 += 1;
+            y1 -= 1;
+            dy += a as i32;
+            err += dy;
+        }
+        if e2 >= dx || 2*err > dy {
+            x0 += 1;
+            x1 -= 1;
+            dx += b1 as i32;
+            err += dx;
+        }
+
+        if x0 > x1 {
+            break;
+        }
+    }
+
+    /*
+    while (y0 as i32 - y1 as i32) < b as i32 {
+        img.draw((x0-1) as f64 / dimensions.0 as f64, y0 as f64 / dimensions.1 as f64, 255);
+        img.draw((x1+1) as f64 / dimensions.0 as f64, y0 as f64 / dimensions.1 as f64, 255);
+        y0 += 1;
+        img.draw((x0-1) as f64 / dimensions.0 as f64, y1 as f64 / dimensions.1 as f64, 255);
+        img.draw((x1+1) as f64 / dimensions.0 as f64, y1 as f64 / dimensions.1 as f64, 255);
+        y1 -= 1;
+    }*/
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 extern crate image;
 
-use image::{ImageBuffer, Luma};
-
 mod canvas;
 
-use canvas::{Canvas, ImageCanvas, Line, Shape};
+use canvas::{Canvas, ImageCanvas, Line, Shape, Ellipse};
 
 fn main() {
     let mut img = ImageCanvas::blank(512, 512);
@@ -15,14 +13,14 @@ fn main() {
         x1: 0.9,
         y1: 0.7
     };
-    let line2 = Line {
-        x0: 0.5,
+    let ellipse1 = Ellipse {
+        x0: 0.1,
         y0: 0.1,
         x1: 0.5,
         y1: 0.9
     };
     scene.push(Shape::Line(line1));
-    scene.push(Shape::Line(line2));
+    scene.push(Shape::Ellipse(ellipse1));
     img.render(scene);
     img.save("line.png").unwrap();
 }
